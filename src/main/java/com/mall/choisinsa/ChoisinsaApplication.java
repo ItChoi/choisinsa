3eeee79e2f493cf09c78b447dbf791009bedb7d1
ChoiSangHyun <itchoi0429@gmail.com>
2022-08-04 00:52:18 +0900

    project init

---
+++ src/main/java/com/mall/choisinsa/ChoisinsaApplication.java
@@ -0,0 +1,13 @@
+package com.mall.choisinsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoisinsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChoisinsaApplication.class, args);
+    }
+
+}
